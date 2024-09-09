357073c5cd2f17694f0144197b010cdbeea743fd
khaisarsgithub <mdkhaisars118@gmail.com>
2024-09-09 18:05:13 +0530

    Initial Commit

---
+++ Apprunnertest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Apprunnertest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Apprunnertest.settings')
+
+application = get_asgi_application()
